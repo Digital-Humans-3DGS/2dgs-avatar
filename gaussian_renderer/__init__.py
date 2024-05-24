9a0f4c98b2615fa49dd88778a16b2399c1557c4e
Li Huapeng <86916640+IronLu11@users.noreply.github.com>
2024-05-24 14:37:14 +0200

    2dgs

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -12,6 +12,8 @@
 import torch
 import math
 from diff_surfel_rasterization import GaussianRasterizationSettings, GaussianRasterizer
+# new
+from utils.point_utils import depth_to_normal
 
 def render(data,
            iteration,
@@ -114,7 +116,7 @@ def render(data,
 
     # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
     # They will be excluded from value updates used in the splitting criteria.
-    return {"deformed_gaussian": pc,
+    rets = {"deformed_gaussian": pc,
             "render": rendered_image,
             "viewspace_points": screenspace_points,
             "visibility_filter" : radii > 0,
@@ -122,3 +124,45 @@ def render(data,
             "loss_reg": loss_reg,
             "opacity_render": opacity_image,
             }
+    
+    # additional regularizations
+    render_alpha = allmap[1:2]
+
+    # get normal map
+    render_normal = allmap[2:5]
+    render_normal = (render_normal.permute(1,2,0) @ (data.world_view_transform[:3,:3].T)).permute(2,0,1)
+    
+    # get median depth map
+    render_depth_median = allmap[5:6]
+    render_depth_median = torch.nan_to_num(render_depth_median, 0, 0)
+
+    # get expected depth map
+    render_depth_expected = allmap[0:1]
+    render_depth_expected = (render_depth_expected / render_alpha)
+    render_depth_expected = torch.nan_to_num(render_depth_expected, 0, 0)
+    
+    # get depth distortion map
+    render_dist = allmap[6:7]
+
+    # psedo surface attributes
+    # surf depth is either median or expected by setting depth_ratio to 1 or 0
+    # for bounded scene, use median depth, i.e., depth_ratio = 1; 
+    # for unbounded scene, use expected depth, i.e., depth_ration = 0, to reduce disk anliasing.
+    surf_depth = render_depth_expected * (1-pipe.depth_ratio) + (pipe.depth_ratio) * render_depth_median
+    
+    # assume the depth points form the 'surface' and generate psudo surface normal for regularizations.
+    surf_normal = depth_to_normal(data, surf_depth)
+    surf_normal = surf_normal.permute(2,0,1)
+    # remember to multiply with accum_alpha since render_normal is unnormalized.
+    surf_normal = surf_normal * (render_alpha).detach()
+
+
+    rets.update({
+            'rend_alpha': render_alpha,
+            'rend_normal': render_normal,
+            'rend_dist': render_dist,
+            'surf_depth': surf_depth,
+            'surf_normal': surf_normal,
+    })
+
+    return rets
